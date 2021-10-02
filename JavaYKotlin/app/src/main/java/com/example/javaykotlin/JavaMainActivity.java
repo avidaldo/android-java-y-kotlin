a8a724f34865b0f13ee3dde8189e282d2f0533b4
avidaldo <avd.formacion@gmail.com>
2021-10-02 22:01:27 +0200

    Añadidos Intents para poder saltar entre las Activities

--- JavaYKotlin/app/src/main/java/com/example/javaykotlin/JavaMainActivity.java
+++ JavaYKotlin/app/src/main/java/com/example/javaykotlin/JavaMainActivity.java
@@ -2,7 +2,10 @@ package com.example.javaykotlin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class JavaMainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class JavaMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonMAJ = findViewById(R.id.buttonMAJ);
+
+        buttonMAJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* Declaramos un objeto intent, una abstracción de la intención de cambiar de actividad.
+                Este método recibe:
+                    - El contexto de partida: la activity en que está. Utilizamos ".this" para
+                    referirnos al objeto de la clase que encierra (enclosing) nuestra clase anónima.
+                    - La clase de la Activity que ha de ser creada. Como esta activity todavía no se puede
+                    instaciar, se usa la sintaxis .class (reflection).
+                */
+                Intent intent = new Intent(JavaMainActivity.this, KotlinMainActivity.class);
+
+                /* Llamamos al método que inicia una activity a partir del intent */
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- JavaYKotlin/app/src/main/java/com/example/javaykotlin/KotlinMainActivity.kt
+++ JavaYKotlin/app/src/main/java/com/example/javaykotlin/KotlinMainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.javaykotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 
 /**
@@ -13,5 +15,16 @@ class KotlinMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_main)
+
+        val buttonMAK = findViewById<Button>(R.id.buttonMAK)
+
+        buttonMAK.setOnClickListener {
+            val intent = Intent(
+                this@KotlinMainActivity,  // https://kotlinlang.org/docs/this-expressions.html#qualified
+                JavaMainActivity::class.java
+            )
+            startActivity(intent)
+        }
+
     }
 }
